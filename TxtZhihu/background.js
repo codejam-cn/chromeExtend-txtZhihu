90097491cc5d297979aeb53e5c1ff9da151c7388
festone000 <festone000@hotmail.com>
2016-11-18 18:10:42 +0800

    代码改良了许多。持续改进。

--- TxtZhihu/background.js
+++ TxtZhihu/background.js
@@ -1,33 +1,50 @@
+﻿/**
+ * ZHAOs @2016年11月18日1643:35:
+ */
+var showForPages = ["https://www.zhihu.com/*", "http://www.zhihu.com/*"];
 
-var showForPages = ["https://www.zhihu.com/*"];
 
+//一级菜单产生
 var lvl1ContextItemId = chrome.contextMenus.create({
     "title": "txt Zhihu",
     "documentUrlPatterns": showForPages,
     "contexts": ["page"]
 });
 
-var pagePartions = ["top", "profile", "right"];
-var lvl2ContextItemIdArr = [];
 
+
+//二级菜单产生
+var pagePartions = ["top", "profile", "right", "", "portrait", "thumbnail", "all"];
+var lvl2ContextItemIdArr = [];
 for (var i = 0; i < pagePartions.length; i++) {
-    var itemId = chrome.contextMenus.create({
+    var obj = (pagePartions[i] === "") ? {
+        type: "separator",
+        "parentId": lvl1ContextItemId
+    } : {
         "title": "toggle " + pagePartions[i],
         "parentId": lvl1ContextItemId,
         "documentUrlPatterns": showForPages,
         "onclick": mycallback
-    });
+    }
+
+    var itemId = chrome.contextMenus.create(obj);
     lvl2ContextItemIdArr.push(itemId);
 }
 
+//获得菜单项索引
+function getItemIndex(item, itemArr) {
+    for (var i = 0; i < itemArr.length; i++) {
+        if (item == itemArr[i]) {
+            return i;
+        }
+    }
+    return NaN;
+}
 
+//执行函数
 function mycallback(info, tab) {
-    var lvl2ContextItemIdJoin = lvl2ContextItemIdArr.join(',');
-    var curItemId = info.menuItemId;
-
-    var msg = curItemId + "&" + lvl2ContextItemIdJoin;
-
-    chrome.tabs.sendRequest(tab.id, { greeting: msg }, function (response) {
-        console.log(response.farewell);
+    var clickedIndex = getItemIndex(info.menuItemId, lvl2ContextItemIdArr); //0 1 2 3 4 5
+    chrome.tabs.sendRequest(tab.id, { greeting: clickedIndex }, function (response) {
+        //console.log(response.farewell);
     });
 }
--- TxtZhihu/front.js
+++ TxtZhihu/front.js
@@ -1,45 +1,92 @@
 ﻿
-
-chrome.extension.onRequest.addListener(
-  function (request, sender, sendResponse) {
-      console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-
-      var msg = request.greeting;
-      var itemId = msg.split('&')[0];
-      var itemIds = (msg.split('&')[1]).split(',');
-
-      var index = getItemIndex(itemId, itemIds);
-      switch (index) {
-          case 0:
-              toggleItem($(".zu-top"));
-              break;
-          case 1:
-              toggleItem($(".HomeEntry"));
-              break;
-          case 2:
-              toggleItem($(".zu-main-sidebar"));
-              break;
-          default:
-              break;
-      }
-      sendResponse({ farewell: index });
-  });
-
-
-function getItemIndex(item, itemArr) {
-    for (var i = 0; i < itemArr.length; i++) {
-        if (item === itemArr[i]) {
-            return i;
+var txtZhihu = {
+    $top: $(".zu-top"), //1
+    $HomeEntry: $(".HomeEntry"), //2
+    $rightSlide: $(".zu-main-sidebar"), //4
+    $portrait: $(".side-topic-avatar"), //8
+    $thumbnail: $(".zh-lightbox-thumb"), //16
+    $domArr: [],
+    cookieVal:"",
+    init: function () {
+        var that = this;
+        that.$domArr = [that.$top, that.$HomeEntry, that.$rightSlide, that.$portrait, that.$thumbnail];
+        var cookie = that.getCookie(); //txtZhihuCookie = 31     
+        that.cookieVal = cookie.length > 0 ? parseInt(cookie.split('=')[1]) : 0;
+       
+        that.hideDoms();
+        that.addMsgListener();
+        that.scrollEvtReg();
+    },
+    hideDoms: function() {
+        var that = this;
+        for (var i = 0; i < that.$domArr.length; i++) {
+            var binaryIndex = Math.pow(2, i);
+            if ((that.cookieVal & binaryIndex) === 0) {
+                that.$domArr[i].hide();
+            }
         }
+    },
+    getCookie: function () {
+        var reg = /txtZhihuCookie=\d+/;
+        var arr = document.cookie.match(reg);
+        return (arr.length > 0) ? arr[0] : ""; //txtZhihuCookie = 31     
+    },
+    addMsgListener: function () {
+        var that = this;
+        chrome.extension.onRequest.addListener(
+            function (request, sender, sendResponse) {
+                var itemIndex = request.greeting;//0 1 2 3 4 5
+                that.toggleItem(itemIndex);
+                //sendResponse({ farewell: index });
+            });
+    },
+    toggleAll: function () {
+        var that = this;
+        var $all = that.$top.add(that.$HomeEntry).add(that.$rightSlide).add(that.$portrait).add(that.$portrait).add(that.$thumbnail);
+        var display = $all.css("display");
+        if (display === "none") {
+            that.cookieVal = that.cookieVal | 32;
+            $all.show();
+        } else {
+            that.cookieVal = that.cookieVal ^ 32;
+            $all.hide();
+        }
+        //此处设置cookie
+        //存入cookie的是要显示的元素的映射index
+        document.cookie = "txtZhihuCookie=" + that.cookieVal;
+    },
+    toggleSingle: function (index) {
+        var that = this;
+        var $dom = that.$domArr[index];
+        var binaryFlag = Math.pow(2, index);
+        var display = $dom.css("display");
+        if (display === "none") {
+            that.cookieVal = that.cookieVal | binaryFlag;
+            $dom.show();
+        } else {
+            that.cookieVal = that.cookieVal ^ binaryFlag;
+            $dom.hide();
+        }
+        //此处设置cookie
+        //存入cookie的是要显示的元素的映射index
+        document.cookie = "txtZhihuCookie=" + that.cookieVal;
+    },
+    // index => 0 1 2 3 4 5
+    toggleItem: function (index) {
+        var that = this;
+        if (index >= 0 && index <= 4) {
+            that.toggleSingle(index);
+        } else {
+            that.toggleAll();
+        }
+    },
+    scrollEvtReg: function() {
+        $(window).scroll(function() {
+            console.log(99)
+        });
     }
-    return NaN;
-}
+};
 
-function toggleItem($item) {
-    var display = $item.css("display");
-    if (display === "none") {
-        $item.show();
-    } else {
-        $item.hide();
-    }
-}
+$(document).ready(function () {
+    txtZhihu.init();
+});
